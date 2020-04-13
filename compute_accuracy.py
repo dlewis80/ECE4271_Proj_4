13974b9dd34726df76e428a986a6be0328147516
Raul <31164367+rmartincelaya@users.noreply.github.com>
2020-04-14 01:07:27 +0200

    Added script to calculate the accuracy

---
+++ compute_accuracy.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import argparse
+from argparse import RawTextHelpFormatter
+
+
+
+description= r'''
+
+This program computes the accuracy of a sound classifier.
+The inputs are the directories containing the text files with
+the labels for each audio file.
+
+Both the human-labeled directory and the machine-labeled directory
+must have the same number of files and with the sames names. An audio file
+called example.wav should have an example.txt file in the human-labeled 
+directory and an example.txt file in the machine-labeled directory.
+
+The format of each file must be one line per labeled fragment with 
+the starting time and the ending time of the fragment in milliseconds
+and the label separated by \t character . For example:
+
+    1290    8999    car
+    12335   22098   talking
+    45566   50003   noise
+    67999   90010   car
+
+A fragment is considered to be well classified (true positive) if
+the detected fragment interval overlaps with the real fragment.
+
+*** This program does not check the input files. Giving two directories with 
+files with different names or bad formatted will give unexpected results*** 
+
+'''
+parser = argparse.ArgumentParser(description=description,formatter_class=RawTextHelpFormatter)
+parser.add_argument('human_directory',metavar='human_labels_directory', \
+                    type=str,help="Directory with the real labels")
+parser.add_argument('machine_directory',metavar='machine_labels_directory', \
+                    type=str,help="Directory with the classifier labels")
+
+args = parser.parse_args()
+human_directory = args.human_directory
+machine_directory = args.machine_directory
+human_files = []
+machine_files =[]
+
+for file in os.listdir(human_directory):
+    if file.endswith(".txt"):
+        human_files.append(os.path.join(human_directory,file))
+
+for file in os.listdir(machine_directory):
+    if file.endswith(".txt"):
+        machine_files.append(os.path.join(machine_directory,file))
+
+human_files.sort()
+machine_files.sort()
+
+classes_stats ={}
+human_times = []
+machine_times = []
+
+if (len(human_files) != len(machine_files)):
+    print("Incorrect input directories")
+    sys.exit()
+
+for i in range(len(human_files)):
+    human_times.clear()
+    machine_times.clear()
+    with open(human_files[i]) as fh, open(machine_files[i]) as fm:
+        for line in fh:
+            tokens = line.split("\t")
+            human_times.append([float(tokens[0]), float(tokens[1]), tokens[2].rstrip(),False])
+        for line in fm:
+            tokens = line.split("\t")
+            machine_times.append([float(tokens[0]), float(tokens[1]), tokens[2].rstrip(),False])
+    
+    for k in human_times:
+        if (k[2] not in classes_stats.keys()):
+            classes_stats[k[2]] ={'false_positives':0,'true_positives':0,"count":0}
+
+    for l in machine_times:
+        if (k[2] not in classes_stats.keys()):
+            classes_stats[k[2]] ={'false_positives':0,'true_positives':0,"count":0}
+        
+    for k in human_times:
+        for l in machine_times:
+            if k[2] == l[2]:
+                #Checking if two fragments with the same label overlap 
+                if (k[0] <= l[1] and k[1] >= l[0]):
+                    k[3] = True
+                    l[3] = True
+
+    #Counting the number of overlapping files and stats
+    for k in human_times:
+        classes_stats[k[2]]["count"] = classes_stats[k[2]]["count"] +1
+        if (k[3] == True):
+            classes_stats[k[2]]["true_positives"] = classes_stats[k[2]]["true_positives"]+1
+    for l in machine_times:
+        if (l[3] == False):
+            classes_stats[l[2]]["false_positives"] = classes_stats[l[2]]["false_positives"]+1
+    
+total_n = 0
+total_tp = 0
+for k in classes_stats:
+    tp = classes_stats[k]["true_positives"]
+    fp = classes_stats[k]["false_positives"]
+    cnt = classes_stats[k]["count"]
+    total_n = total_n+cnt
+    total_tp = total_tp+tp
+
+    print ("Class: "+k)
+    print ("    Number of cases: "+str(cnt))
+    print ("    True positives: "+str(tp))
+    print ("    False positives: "+str(fp))
+    print ("    True positive rate: "+ str(float(tp/cnt)))
+    print("")
+
+print("General Accuracy: "+str(float(total_tp/total_n)))
+print("")
